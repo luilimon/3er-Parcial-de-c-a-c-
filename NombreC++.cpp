456c4e29010a0477f9e3bea8e9086a4962193876
Luisa Lima <168607491+luilimon@users.noreply.github.com>
2024-06-03 15:35:28 -0600

    Create NombreC++.cpp

---
+++ NombreC++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string nombre;
+    std::cout << "Hola, buenos días. Por favor, introduce tu nombre\n";
+
+    std::cin >> nombre;
+
+    std::cout << "\nHola " << nombre << "\n";
+
+    return 0;
+}
+
+
