45436701de168c835d2870a5b0557808da104779
Luisa Lima <168607491+luilimon@users.noreply.github.com>
2024-06-03 15:44:32 -0600

    Create MayorC++.cpp

---
+++ MayorC++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    int num1, num2;
+
+    std::cout << "Ingresa el primer numero: ";
+    std::cin >> num1;
+    std::cout << "Ingresa tu segundo numero: ";
+    std::cin >> num2;
+
+    if (num1 == num2) {
+        std::cout << "Los numeros son iguales." << std::endl;
+    } else if (num1 > num2) {
+        std::cout << "El primer numero es mayor que el segundo." << std::endl;
+    } else {
+        std::cout << "El segundo numero es mayor que el primero." << std::endl;
+    }
+
+    return 0;
+}
+
+
